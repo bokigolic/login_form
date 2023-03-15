978a925c8973ae2463f74ea39ae6ff80f8662034
bokigolic <bokigolic32@gmail.com>
2023-03-14 23:20:37 -0400

    update db

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,18 +1,16 @@
 import React, { useState } from 'react';
-
 import { database } from '../firebase'
 import { ref, push, child, update } from "firebase/database";
 
-const RegistrationForm = () => {
+function RegistrationForm() {
 
-  const [firstName, setFirstName] = useState(null);
-  const [lastName, setLastName] = useState(null);
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
-  const [confirmPassword, setConfirmPassword] = useState(null)
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-
-  const handleChange = (e) => {
+  const handleInputChange = (e) => {
     const { id, value } = e.target;
     if (id === "firstName") {
       setFirstName(value);
@@ -32,81 +30,40 @@ const RegistrationForm = () => {
 
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault(); // prevent default form submission behavior
+    console.log(firstName, lastName, email, password, confirmPassword);
+    // add code to save user data to database
+  }
 
-const handleSubmit = () =>{
-    let obj = {
-            firstName : firstName,
-            lastName:lastName,
-            email:email,
-            password:password,
-        confirmPassword:confirmPassword,
-        }       
-    const newPostKey = push(child(ref(database), 'posts')).key;
-    const updates = {};
-    updates['/' + newPostKey] = obj
-    return update(ref(database), updates);
-}
   return (
     <div className="form">
-      <div className="form-body">
+      <form>
         <div className="username">
-          <label className="form__label" for="firstName">First Name </label>
-          <input className="form__input"
-            type="text"
-            id="firstName"
-            placeholder="First Name"
-            value={firstName}
-            onChange={handleChange}
-          />
+          <label className="form__label" htmlFor="firstName">First Name </label>
+          <input className="form__input" type="text" value={firstName} onChange={(e) => handleInputChange(e)} id="firstName" placeholder="First Name" />
         </div>
         <div className="lastname">
-          <label className="form__label" for="lastName">Last Name </label>
-          <input type="text"
-            name=""
-            id="lastName"
-            value={lastName}
-            onChange={handleChange}
-            className="form__input"
-            placeholder="LastName"
-          />
+          <label className="form__label" htmlFor="lastName">Last Name </label>
+          <input type="text" name="" id="lastName" value={lastName} className="form__input" onChange={(e) => handleInputChange(e)} placeholder="LastName" />
         </div>
         <div className="email">
-          <label className="form__label" for="email">Email </label>
-          <input type="email"
-            className="form__input"
-            placeholder="Email"
-            id="email"
-            value={email}
-            onChange={handleChange}
-          />
+          <label className="form__label" htmlFor="email">Email </label>
+          <input type="email" id="email" className="form__input" value={email} onChange={(e) => handleInputChange(e)} placeholder="Email" />
         </div>
         <div className="password">
-          <label className="form__label" for="password">Password </label>
-          <input
-            className="form__input"
-            type="password"
-            id="password"
-            placeholder="Password"
-            value={password}
-            onChange={handleChange}
-          />
+          <label className="form__label" htmlFor="password">Password </label>
+          <input className="form__input" type="password" id="password" value={password} onChange={(e) => handleInputChange(e)} placeholder="Password" />
         </div>
         <div className="confirm-password">
-          <label className="form__label"
-            for="confirmPassword">Confirm Password </label>
-          <input className="form__input"
-            type="password"
-            id="confirmPassword"
-            placeholder="Confirm Password"
-            onChange={handleChange}
-            value={confirmPassword}
-          />
+          <label className="form__label" htmlFor="confirmPassword">Confirm Password </label>
+          <input className="form__input" type="password" id="confirmPassword" value={confirmPassword} onChange={(e) => handleInputChange(e)} placeholder="Confirm Password" />
         </div>
-      </div>
-      <div class="footer">
-        <button type="submit" onClick={handleSubmit} class="btn">Register</button>
-      </div>
+        <button onClick={(event) => handleSubmit(event)} type="submit" className="btn">Register</button>
+      </form>
     </div>
+
   )
 }
+
 export default RegistrationForm
